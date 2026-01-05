3ae3f5a8eff41feafcbe99a0ae04e76ffcd62f7f
deerarif <deerarif@protonmail.com>
2026-01-05 22:42:17 +0800

    update makedb

--- Backend/src/db/make_db.py
+++ Backend/src/db/make_db.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import create_engine, Column, Integer, String, Float, URL
 from sqlalchemy.orm import sessionmaker, declarative_base
 from db_model import Assets
 from db_model import Documents
@@ -14,8 +14,23 @@ DB_NAME = os.getenv("DB_NAME")
 PASSWORD = os.getenv("PASSWORD")
 
 
+# load_dotenv()
+USERNAME = os.getenv("USERNAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+PASSWORD = os.getenv("PASSWORD")
+
+
+url = URL.create(
+    drivername="mysql+pymysql",
+    username=USERNAME,
+    password=PASSWORD,
+    host=DB_HOST,
+    port=3306,
+    database=DB_NAME,
+)
 engine = create_engine(
-    f"mysql+pymysql://{USERNAME}:{PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4",
+    url,
     echo=True,
 )
 Base.metadata.create_all(engine)
